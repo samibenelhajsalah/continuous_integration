632498fd7cea9e7f0352bae566d267d70279c407
BEN ELHAJ SALAH Sami <sami.bhs.91@gmail.com>
2023-06-13 23:10:32 +0200

    fix bug in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, CI with GitHub Actions! I m in main function (try ci ) << std::endl;
+    std::cout << "Hello, CI with GitHub Actions! I m in main function (try ci )" << std::endl;
     return 0;
 }
