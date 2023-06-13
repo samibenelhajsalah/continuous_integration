3f555299332e12a50fd89f2fef9e7dab7e44c47f
BEN ELHAJ SALAH Sami <sami.bhs.91@gmail.com>
2023-06-13 23:17:20 +0200

    change main c++

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
+    std::cout << "Hello Sami" << std::endl;
     std::cout << "Hello, CI with GitHub Actions! I m in main function (try ci )" << std::endl;
     return 0;
 }
